9829b44c3c191e6c75f3425415feb20f4d7be88b
postgres <nasir.mycloud@gmail.com>
2021-07-14 12:57:00 +0530

    SELECT STATEMENT FROM QUERY EXAMPLE & USES

---
+++ Section 1/select.sql
@@ -0,0 +1,4 @@
+Retrieving Data using SELECT Statement
+--1
+SELECT * FROM CUSTOMERS; --It will list out all data in the table customers,it will give all column in the table customers.Here * means list out all columns data in the table CUSTOMERS.In postgresql,you can write query in lowercase or uppercase.it does not matter ie case insensitive.
+
